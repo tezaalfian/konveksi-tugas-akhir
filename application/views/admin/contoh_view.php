cee58972702a917cc15cf7dc7f6cd70886928d18
Teza Alfian <tezaalfian2916@gmail.com>
2019-05-27 23:25:18 +0700

    1

---
+++ application/views/admin/contoh_view.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contoh</title>
+    <?php $this->load->view('partial/admin/css');?>
+</head>
+<body>
+<!-- SIDEBAR -->
+    <?php $this->load->view('partial/admin/sidebar');?>
+
+    <div id="right-panel" class="right-panel">
+    <!-- HEADER -->
+        <?php $this->load->view('partial/admin/header');?>
+
+        <div class="breadcrumbs">
+            <div class="breadcrumbs-inner">
+                <div class="row m-0">
+                    <div class="col-sm-4">
+                        <div class="page-header float-left">
+                            <div class="page-title">
+                                <h1>Dashboard</h1>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-8">
+                        <div class="page-header float-right">
+                            <div class="page-title">
+                                <ol class="breadcrumb text-right">
+                                    <li><a href="#">Dashboard</a></li>
+                                    <li><a href="#">Table</a></li>
+                                    <li class="active">Data table</li>
+                                </ol>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    <!-- END CONTENT -->
+
+    <!-- FOOTER -->
+        <?php $this->load->view('partial/admin/footer');?>
+    </div>
+
+<!-- LIBRARY JS -->
+    <?php $this->load->view('partial/admin/js');?>
+    
+</body>
+</html>
